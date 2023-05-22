168616a1fa7ec64c0c9634abbd860875d2210e7d
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-05-22 13:29:07 +0200

    Revamp releaseSpriteFramesCache (#168)
    * Now loads/unloads frame data dynamically for ALL `TextureAtlasSprites`
    * Data not referenced for 20 consecutive client ticks is unloaded
    * Synchronize some methods in `FramesTextureData` to help prevent crashes
    
    (cherry picked from commit 8b423982e45039d9cc51ffa7ce2114d054d5dd18)

--- src/main/java/mirror/normalasm/proxy/ClientProxy.java
+++ src/main/java/mirror/normalasm/proxy/ClientProxy.java
@@ -1,8 +1,6 @@
 package mirror.normalasm.proxy;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.resources.IReloadableResourceManager;
 import net.minecraftforge.client.resource.ISelectiveResourceReloadListener;
 import net.minecraftforge.client.resource.VanillaResourceType;
@@ -13,17 +11,15 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
-import pl.asie.foamfix.shared.FoamFixShared;
-import slimeknights.tconstruct.library.client.texture.AbstractColoredTexture;
-import mirror.normalasm.LoliLogger;
-import mirror.normalasm.LoliReflector;
-import mirror.normalasm.bakedquad.LoliVertexDataPool;
-import mirror.normalasm.client.models.bucket.LoliBakedDynBucket;
+import mirror.normalasm.NormalLogger;
+import mirror.normalasm.NormalReflector;
+import mirror.normalasm.bakedquad.NormalVertexDataPool;
+import mirror.normalasm.client.models.bucket.NormalBakedDynBucket;
 import mirror.normalasm.client.screenshot.ScreenshotListener;
 import mirror.normalasm.client.sprite.FramesTextureData;
 import mirror.normalasm.common.modfixes.qmd.QMDEventHandler;
-import mirror.normalasm.config.LoliConfig;
-import mirror.normalasm.core.LoliTransformer;
+import mirror.normalasm.config.NormalConfig;
+import mirror.normalasm.core.NormalTransformer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,19 +29,6 @@ import java.util.Map;
 public class ClientProxy extends CommonProxy {
 
     public static final List<Runnable> refreshAfterModels = new ArrayList<>();
-    public static final boolean flushTinkerSpriteFrameTextureData;
-
-    static {
-        boolean static$flushTinkerSpriteFrameTextureData = true;
-        if (Loader.isModLoaded("tconstruct") && Loader.isModLoaded("foamfix")) {
-            if (FoamFixShared.config.clDynamicItemModels) {
-                static$flushTinkerSpriteFrameTextureData = false;
-            }
-        }
-        flushTinkerSpriteFrameTextureData = static$flushTinkerSpriteFrameTextureData;
-    }
-
-    public static boolean canReload = true;
 
     @Override
     public void preInit(FMLPreInitializationEvent event) {
@@ -90,22 +73,6 @@ public class ClientProxy extends CommonProxy {
                     NormalBakedDynBucket.flippedBaseQuads.clear();
                     NormalBakedDynBucket.coverQuads.clear();
                     NormalBakedDynBucket.flippedCoverQuads.clear();
-                }
-                if (NormalConfig.instance.releaseSpriteFramesCache) {
-                    canReload = false;
-                    try {
-                        for (TextureAtlasSprite sprite : ((Map<String, TextureAtlasSprite>) NormalReflector.resolveFieldGetter(TextureMap.class, "mapRegisteredSprites", "field_110574_e").invoke(Minecraft.getMinecraft().getTextureMapBlocks())).values()) {
-                            if (!sprite.hasAnimationMetadata()) {
-                                if (!flushTinkerSpriteFrameTextureData && sprite instanceof AbstractColoredTexture) {
-                                    continue;
-                                }
-                                sprite.setFramesTextureData(new FramesTextureData(sprite));
-                            }
-                        }
-                    } catch (Throwable e) {
-                        e.printStackTrace();
-                    }
-                    canReload = true;
                 }
                 if (!NormalTransformer.isOptifineInstalled && NormalConfig.instance.vertexDataCanonicalization) {
                     NormalVertexDataPool.invalidate();
