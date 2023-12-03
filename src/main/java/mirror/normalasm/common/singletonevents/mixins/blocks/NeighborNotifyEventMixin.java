LoliKingdom/LoliASM

Implement BlockEvent stuff in NeighborNotifyEvent for now

src/main/java/zone/rong/loliasm/common/singletonevents/mixins/blocks/NeighborNotifyEventMixin.java
@@ -1,26 +1,42 @@
 package zone.rong.loliasm.common.singletonevents.mixins.blocks;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.BlockEvent.NeighborNotifyEvent;
-import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import org.spongepowered.asm.mixin.*;
 import zone.rong.loliasm.common.singletonevents.IRefreshEvent;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.lang.ref.WeakReference;
 import java.util.EnumSet;
 
 @Mixin(NeighborNotifyEvent.class)
-public class NeighborNotifyEventMixin extends Event implements IRefreshEvent {
+public class NeighborNotifyEventMixin extends BlockEvent implements IRefreshEvent {
 
     @Shadow @Final @Mutable private EnumSet<EnumFacing> notifiedSides;
     @Shadow @Final @Mutable private boolean forceRedstoneUpdate;
 
-    @Unique private EventPriority loliPriority = null;
+    @Unique private EventPriority loliPriority;
+    @Unique private WeakReference<World> loliWorldRef;
+
+    NeighborNotifyEventMixin(World world, BlockPos pos, IBlockState state) {
+        super(world, pos, state);
+        throw new AssertionError();
+    }
 
     @Override
-    public void beforeNeighborNotify(EnumSet<EnumFacing> notifiedSides, boolean forceRedstoneUpdate) {
+    public World getWorld() {
+        return this.loliWorldRef.get();
+    }
+
+    @Override
+    public void beforeNeighborNotify(World world, EnumSet<EnumFacing> notifiedSides, boolean forceRedstoneUpdate) {
+        this.loliWorldRef = new WeakReference<>(world);
         this.notifiedSides = notifiedSides;
         this.forceRedstoneUpdate = forceRedstoneUpdate;
     }

mirrorcult/NormalASM

(cherry picked from commit ff110d56c8d9bba57e370ce874aaa83b2cfdc3bc)

src/main/java/mirror/normalasm/common/singletonevents/mixins/blocks/NeighborNotifyEventMixin.java
@@ -1,26 +1,42 @@
 package mirror.normalasm.common.singletonevents.mixins.blocks;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.BlockEvent.NeighborNotifyEvent;
-import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import org.spongepowered.asm.mixin.*;
 import mirror.normalasm.common.singletonevents.IRefreshEvent;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.lang.ref.WeakReference;
 import java.util.EnumSet;
 
 @Mixin(NeighborNotifyEvent.class)
-public class NeighborNotifyEventMixin extends Event implements IRefreshEvent {
+public class NeighborNotifyEventMixin extends BlockEvent implements IRefreshEvent {
 
     @Shadow @Final @Mutable private EnumSet<EnumFacing> notifiedSides;
     @Shadow @Final @Mutable private boolean forceRedstoneUpdate;
 
-    @Unique private EventPriority normalPriority = null;
+    @Unique private EventPriority normalPriority;
+    @Unique private WeakReference<World> normalWorldRef;
+
+    NeighborNotifyEventMixin(World world, BlockPos pos, IBlockState state) {
+        super(world, pos, state);
+        throw new AssertionError();
+    }
 
     @Override
-    public void beforeNeighborNotify(EnumSet<EnumFacing> notifiedSides, boolean forceRedstoneUpdate) {
+    public World getWorld() {
+        return this.normalWorldRef.get();
+    }
+
+    @Override
+    public void beforeNeighborNotify(World world, EnumSet<EnumFacing> notifiedSides, boolean forceRedstoneUpdate) {
+        this.normalWorldRef = new WeakReference<>(world);
         this.notifiedSides = notifiedSides;
         this.forceRedstoneUpdate = forceRedstoneUpdate;
     }